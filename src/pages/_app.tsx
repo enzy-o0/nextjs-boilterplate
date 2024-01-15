3b586a12cd3b908827595a63e23c006a0d487b21
enzy-o0 <oej3659@gmail.com>
2024-01-15 14:17:48 +0900

    feat: 임시 데이터 UI - MUI 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import apolloClient from '@/app/lib/apolloClient';
 import { ApolloProvider } from '@apollo/client';
+import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
+import Container from '@mui/material/Container';
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ApolloProvider client={apolloClient}>
-            <Component {...pageProps} />
+            <AppCacheProvider {...pageProps}>
+                <Container
+                    maxWidth="sm"
+                    style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                >
+                    <Component {...pageProps} />
+                </Container>
+            </AppCacheProvider>
         </ApolloProvider>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,13 @@
 
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CircularProgress from '@mui/material/CircularProgress';
+import Container from '@mui/material/Container';
 
 const Users = gql`
     query getUsers {
@@ -17,18 +24,54 @@ const Users = gql`
     }
 `;
 
+type PostType = {
+    title: string;
+    body: string;
+};
+
+type UserType = {
+    id: string;
+    name?: string;
+    posts: PostType[];
+};
+
 export default function Home() {
     const { data, loading, error } = useQuery(Users);
 
-    if (loading) return <p>Loading...</p>;
+    if (loading) return <CircularProgress />;
     if (error) return <p>error: {error.message}</p>;
 
     return (
-        <>
-            {data?.users.map((e) => {
+        <div>
+            <Typography sx={{ fontSize: 32, textAlign: 'center' }} color="text.secondary" gutterBottom>
+                연습하기
+            </Typography>
+            {data?.users.map((e: UserType) => {
                 console.log(e);
-                return <div key={e.id}>{e.name}</div>;
+                if (!e.name) return false;
+                return (
+                    <Card key={e.id} sx={{ minWidth: 275,  marginTop: '1rem' }}>
+                        <CardContent>
+                            {e.posts.map((post) => (
+                                <>
+                                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                                        {post.title}
+                                    </Typography>
+                                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                        {post.body}
+                                    </Typography>
+                                </>
+                            ))}
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                {e.name}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">더보기</Button>
+                        </CardActions>
+                    </Card>
+                );
             })}
-        </>
+        </div>
     );
 }
